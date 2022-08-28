e5fc3f6c6ea1d25f527948334071baae405fe376
Goga <gogaimerlishvili1@gmail.com>
2022-08-29 01:30:17 +0400

    created api folder for weather api

---
+++ src/Api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "d99e8ac033msh62fd071517a5dc2p10635fjsn005cf13f66aa",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
